831977a82560a47651ec59c51403ae533895dae7
Kaitlyn <Kaitlyn@192.168.0.194>
2012-12-08 16:31:44 -0500

    added maping

--- OperationHotHammer/src/OperationHotHammer/Core/Util/Settings.java
+++ OperationHotHammer/src/OperationHotHammer/Core/Util/Settings.java
@@ -5,10 +5,28 @@
 
 package OperationHotHammer.Core.Util;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Settings {
     
     public static final float GRID_SPACE_SIZE = 10.0f;
     public static final float PLAYER_SIZE = GRID_SPACE_SIZE/2;
     public static final String ASSETS_DIR = System.getProperty("user.dir") + "\\src\\OperationHotHammer\\Assets\\";
+    public static final Map<String, String> DATA_MAP;
+    static {
+        Map<String, String> map = new HashMap<>();
+        map.put("grass1", "grass1.dat");
+        map.put("grass2", "grass2.dat");
+        DATA_MAP = Collections.unmodifiableMap(map);
+    }
+    public static final Map<String, String> TEXTURE_MAP;
+    static {
+        Map<String, String> map = new HashMap<>();
+        map.put("grass1", "grass1.dat");
+        map.put("grass2", "grass2.dat");
+        TEXTURE_MAP = Collections.unmodifiableMap(map);
+    }
 }
