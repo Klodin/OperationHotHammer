36549e43e396edde808ac2553c57adfa101317a0
Klodin <klodin@hotmail.com>
2012-12-30 18:06:27 -0500

    added addParser

---
+++ OperationHotHammer/src/OperationHotHammer/Core/Data/DataParser.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OperationHotHammer.Core.Data;
+import OperationHotHammer.Core.GameObjects.Entities.*;
+import OperationHotHammer.Core.GameObjects.Entity;
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Klodin
+ */
+public class DataParser {
+    public static Entity parseFile(File file){
+        Entity entity = null;
+        try {
+            FileReader in = new FileReader(file);
+            Scanner scan = new Scanner(in);
+            while (scan.hasNext()){
+                String command = scan.nextLine();
+                String orders[] = command.split(" ");
+                switch(orders[0]){
+                    case "type":
+                        switch (orders[1]){
+                            case "terrain":
+                                entity = new Terrain();
+                                break;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            scan.close();
+            in.close();
+            
+        } catch(IOException e){
+            
+        }
+        return entity;
+    }
+}
